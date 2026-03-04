eb80b056bd014ac135552dc65251ec055f072b38
Lamberto Martini <89637786+Lambo95@users.noreply.github.com>
2026-03-04 21:14:22 +0100

    Create Esercizi.py

---
+++ Esercizi.py
@@ -0,0 +1,32 @@
+""" print("Lamberto")
+
+nome= input("Inserisci il tuo nome: ")
+eta= int(input("Inserisci la tua eta: "))
+
+print(f"Mi chiamo {nome} e ho {eta} anni, il doppio dei miei anni è {eta*2}")
+ """
+# Esercizio 1 
+
+x = 7
+y = 3
+somma = x + y
+print(somma)
+
+
+# Esercizio 2
+
+prezzo = 19.99
+quantità = 3
+print(prezzo * quantità)
+
+
+# Esercizio 3
+
+x = 10
+print ( x > 5 ) 
+
+# Esercizio 4
+
+a = 5
+b = 2.5
+print(a+b)
